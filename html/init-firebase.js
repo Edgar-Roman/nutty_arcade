e774ddf21ebf725b52b8fe84cb183bb4ed554ac4
deanzhou <dzhouwave@gmail.com>
2022-01-25 11:20:11 -0800

    firebase login v0.1

---
+++ html/init-firebase.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA2JGtnR4lIhp97E_mFzCXU2VgD5a5QseA",
+  authDomain: "nutty-arcade.firebaseapp.com",
+  projectId: "nutty-arcade",
+  storageBucket: "nutty-arcade.appspot.com",
+  messagingSenderId: "922920530610",
+  appId: "1:922920530610:web:f998879b54dab05ba34fab",
+  measurementId: "G-L4S9VFFZTJ"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const analytics = firebase.analytics();
