a0ff825becfb526bde3a32caac012d425f444247
root <dzhouwave@gmail.com>
2022-02-02 09:06:31 -0800

    flask server setup

--- html/app.py
+++ html/app.py
@@ -1,20 +1,17 @@
 # mainly from https://stackoverflow.com/questions/59975596/connect-javascript-to-python-script-with-flask
-from flask import Flask, render_template
-from flask import jsonify
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
-
-# Display your index page
-@app.route("/")
-def index():
-    return render_template('index.html')
+CORS(app)
 
 # A function to add two numbers
 @app.route("/add")
 def add():
     a = request.args.get('a')
     b = request.args.get('b')
-    return jsonify({"result": a+b})
+    return jsonify({"result": int(a)+int(b)})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.debug = True
+    app.run()
