c402939e580e99771208872896c3ef5bb68c9bcb
William <wjwang@stanford.edu>
2022-02-08 19:48:31 -0800

    rewriting backend code to fit integration api

--- fish/game.py
+++ fish/game.py
@@ -1,37 +1,26 @@
-from deck import Deck
-from information import Information
 import random
-from team import Team
+from players import Player
+#from team import Team
 import itertools
 
 class Fish:
-    def __init__(self, team1, team2):
+    # to do: add teams later
+    def __init__(self):
         # initialize deck + game constants
+        self.num_players = 6
         self.num_hs = 9
         self.num_cards_per_hs = 6
         self.can_ask_for_own_card = False
-        self.hand_size_public = False
-        self.total_turns = 0
-        # initialize teams
-        self.team1 = team1
-        self.team2 = team2
-        # initialize players
-        self.players = []
-        self.players.extend(team1.players)
-        self.players.extend(team2.players)
-        self.num_players = len(self.players)
+        self.hand_size_public = True
+        # initialize players and teams
+        self.id2p = {}
+        for id in range(self.num_players):
+            team_id = (2 * id) // self.num_players
+            self.id2p[id] = Player(id, team_id)
+        self.team_scores = (0, 0)
         # setup the game
-        self.initialize_game_info()
-        self.initialize_team_info()
         self.deal_cards()
-        self.current_player = self.players[random.randint(0, self.num_players - 1)]
-
-    def initialize_game_info(self):
-        for player in self.players:
-            player.num_hs = 9
-            player.num_cards_per_hs = 6
-            player.can_ask_for_own_card = False
-            player.hand_size_public = False
+        self.current_player = self.id2p[random.randint(0, self.num_players - 1)]
 
     def deal_cards(self):
         # initialize the cards and shuffle them
@@ -40,59 +29,67 @@ class Fish:
 
         # determine the number of players and the number of cards each player should receive, then distribute
         cards_per_player = self.num_hs * self.num_cards_per_hs / self.num_players
+        for id in range(self.num_players):
+            player = self.id2p[id]
+            player.initialize_hand(cards[int(id * cards_per_player): int((id + 1) * cards_per_player)])
+
+    # integration or smth idk
+    def assignPlayer2ID(self):
+        pass
+
+    """
+    returns value
+    {
+    hand: list(tuples) // TUPLE IS ('SUIT', NUM) e.g. ('C', 4) A - 1, JQK 11-13
+    num_cards: list(int) // LENGTH IS 6
+    score: tuple // LENGTH IS 2
+    turn: int //0-5
+    }
+    """
+    def getGameState(self, id):
+        player = self.id2p[id]
         for i in range(self.num_players):
-            self.players[i].initialize_hand(cards[int(i * cards_per_player): int((i + 1) * cards_per_player)])
+            player = self.id2p[i]
+            hand = player.get_hand() # need to convert cards from fish format into (suit, num) format
+            num_cards = player.num_cards
+        score = self.team_scores
+        turn = self.current_player.id
+        pass
 
-    def initialize_team_info(self):
-        #TODO: teammates
-        for player in self.team1.players:
-            player.teammates = []
-            player.teammates.extend(self.team1.players)
-            player.teammates.remove(player)
-            player.opponents = self.team2.players
-        for player in self.team2.players:
-            player.teammates = []
-            player.teammates.extend(self.team2.players)
-            player.teammates.remove(player)
-            player.opponents = self.team1.players
+    # assert ask is legal
+    def askCard(self, suit, number, id1, id2):
+        card = self.convertToFishCard(suit, number)
+        player_asking = self.id2p[id1]
+        player_questioned = self.id2p[id2]
+        got_card = player_questioned.hasCard(card)
+        for id in range(self.num_players):
+            player = self.id2p[id]
+            player.update(player_asking, card, player_questioned, got_card)
+        if not got_card:
+            self.current_player.id = player_questioned.id
 
+    #id is id corresponding to each card
+    def declareSuit(self, suit, declare_id, id1, id2, id3, id4, id5, id6):
+        ids = [id1, id2, id3, id4, id5, id6]
+        declare_correct = True
+        for value in range(self.num_cards_per_hs):
+            declared_card = (suit, value)
+            declared_player = self.id2p[ids[value]]
+            # need to enforce these ids are on the same team as id_declare
+            declare_correct = declare_correct and declared_player.has_card(declared_card)
+        if declare_correct:
+            self.team_scores[declare_id.team_id] += 1
+        else:
+            self.team_scores[1 - declare_id.team_id] += 1
+        # remove all cards of the half-suit after declaring
+        for id in range(self.num_players):
+            player = self.id2p[id]
+            player.remove_hs(suit)
 
-    def display_all_hands(self):
-        for player in self.players:
-            print(player.get_hand())
+    # should take normal suit/number and convert it into card corresponding to tuple: (half-suit, value)
+    def convertToFishCard(self, suit, number):
+        return card
 
-    # updates info +hands for each player
-    def update_info(self, current_player, card, next_player, got_card):
-        for player in self.players:
-            player.update(current_player, card, next_player, got_card)
-
-    def play(self):
-        # TODO: implement the win condition (i.e. while no team has won 5+ half suits):
-        while self.total_turns < 200:
-            card, next_player = self.current_player.get_next()
-            # the player is out of cards: switch to a teammate if possible
-            if card is None:
-                # TODO: update information that someone is out of cards (in all cases in which it can occur)
-                # for player in self.players:
-                #    if len(player.get_hand) == 0:
-                #        for player_to_update in self.players:
-                #            player_to_update.update_out_of_cards(player, self)
-                # get next card and opponent
-                if next_player is None:
-                    break
-                else:
-                    self.current_player = next_player
-            # the player can ask for a card:
-            else:
-                print("Turn", self.total_turns + 1, ":", self.current_player.name, "asks for the", card, "from", next_player.name)
-                # TODO: assert that current_player can ask for the card from next_player (opposing team)
-                if next_player.has_card(card):
-                    print("hit!")
-                    self.update_info(self.current_player, card, next_player, got_card=True) # event
-                else:
-                    print("miss!")
-                    self.update_info(self.current_player, card, next_player, got_card=False) # event
-                    self.current_player = next_player
-                self.total_turns += 1
-        # end state debugging
-        self.display_all_hands()
+    # should take fish (hs, value) and convert it into card
+    def convertToNormalCard(self, hs, value):
+        return card
