f7fd8a350583da8c5f109d67feff4fb073a068ac
kimchankyo <51008440+kimchankyo@users.noreply.github.com>
2022-02-13 11:20:11 -0800

    testing

--- html/frontend/src/App.jsx
+++ html/frontend/src/App.jsx
@@ -18,136 +18,136 @@ class App extends React.Component {
 
 	render() {
         return (
-			<div>
-				<header>
-					<div id="logo_header">
-						<div id="row1" className="header-background"></div>
-						<div id="row2" className="header-background"></div>
-						<div id="row3" className="header-background"></div>
-						<div id="row4" className="header-background"></div>
-						<div id="row5" className="header-background"></div>
-						<div id="row6" className="header-background"></div>
-						<p id="logo">
-							NUTTY ARCADE
-						</p>
-						<div id="log-in-button-container" className="account-button-container">
-							<button id="log-in-button" className="account-button">
-								LOG IN
-							</button>
-						</div>
-						<div id="register-button-container" className="account-button-container">
-							<button id="register-button" className="account-button">
-								REGISTER
-							</button>
-						</div>
-					</div>
-				</header>
+			// <div>
+			// 	<header>
+			// 		<div id="logo_header">
+			// 			<div id="row1" className="header-background"></div>
+			// 			<div id="row2" className="header-background"></div>
+			// 			<div id="row3" className="header-background"></div>
+			// 			<div id="row4" className="header-background"></div>
+			// 			<div id="row5" className="header-background"></div>
+			// 			<div id="row6" className="header-background"></div>
+			// 			<p id="logo">
+			// 				NUTTY ARCADE
+			// 			</p>
+			// 			<div id="log-in-button-container" className="account-button-container">
+			// 				<button id="log-in-button" className="account-button">
+			// 					LOG IN
+			// 				</button>
+			// 			</div>
+			// 			<div id="register-button-container" className="account-button-container">
+			// 				<button id="register-button" className="account-button">
+			// 					REGISTER
+			// 				</button>
+			// 			</div>
+			// 		</div>
+			// 	</header>
 	
-				<div>
-					<div className="category-container">
-						<div id="card-games" className="page-category">
-							CARD GAMES
-						</div>
-					</div>
-					<div id="card-games" className="game-browser">
-						<div id="game-fish" className="game">
-							<div id="fish-menu" className="game-menu">
-								<div id="fish-thumbnail" className="thumbnail">
+			// 	<div>
+			// 		<div className="category-container">
+			// 			<div id="card-games" className="page-category">
+			// 				CARD GAMES
+			// 			</div>
+			// 		</div>
+			// 		<div id="card-games" className="game-browser">
+			// 			<div id="game-fish" className="game">
+			// 				<div id="fish-menu" className="game-menu">
+			// 					<div id="fish-thumbnail" className="thumbnail">
 	
-								</div>
-							</div>
-							<div className="game-information">
-								<div className="game-title">
-								  FISH
-								</div>
-								<div className="button-container">
-									<button className="play-button">
-										<Link to="/Fish"> PLAY NOW </Link>
-									</button>
-								</div>
-								<div className="player-counter-container">
-									<div className="player-counter-title">
-										PLAYERS ACTIVE
-									</div>
-									<div className="player-counter">
-										5
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<br />
-					<br />
+			// 					</div>
+			// 				</div>
+			// 				<div className="game-information">
+			// 					<div className="game-title">
+			// 					  FISH
+			// 					</div>
+			// 					<div className="button-container">
+			// 						<button className="play-button">
+			// 							<Link to="/Fish"> PLAY NOW </Link>
+			// 						</button>
+			// 					</div>
+			// 					<div className="player-counter-container">
+			// 						<div className="player-counter-title">
+			// 							PLAYERS ACTIVE
+			// 						</div>
+			// 						<div className="player-counter">
+			// 							5
+			// 						</div>
+			// 					</div>
+			// 				</div>
+			// 			</div>
+			// 		</div>
+			// 		<br />
+			// 		<br />
 					
-					<div className="category-container">
-						<div id="board-games" className="page-category">
-							BOARD GAMES
-						</div>
-					</div>
-					<div id="board-games" className="game-browser">
-						<div id="game-pioneers" className="game">
-							<div id="pioneers-menu" className="game-menu">
-								<div id="pioneers-thumbnail" className="thumbnail">
+			// 		<div className="category-container">
+			// 			<div id="board-games" className="page-category">
+			// 				BOARD GAMES
+			// 			</div>
+			// 		</div>
+			// 		<div id="board-games" className="game-browser">
+			// 			<div id="game-pioneers" className="game">
+			// 				<div id="pioneers-menu" className="game-menu">
+			// 					<div id="pioneers-thumbnail" className="thumbnail">
 	
-								</div>
-							</div>
-							<div className="game-information">
-								<div className="game-title">
-									PIONEERS
-								</div>
-								<div className="button-container">
-									<button className="play-button">
-										PLAY NOW
-									</button>
-								</div>
-								<div className="player-counter-container">
-									<div className="player-counter-title">
-										PLAYERS ACTIVE
-									</div>
-									<div className="player-counter">
-										15
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<br />
-					<br />
+			// 					</div>
+			// 				</div>
+			// 				<div className="game-information">
+			// 					<div className="game-title">
+			// 						PIONEERS
+			// 					</div>
+			// 					<div className="button-container">
+			// 						<button className="play-button">
+			// 							PLAY NOW
+			// 						</button>
+			// 					</div>
+			// 					<div className="player-counter-container">
+			// 						<div className="player-counter-title">
+			// 							PLAYERS ACTIVE
+			// 						</div>
+			// 						<div className="player-counter">
+			// 							15
+			// 						</div>
+			// 					</div>
+			// 				</div>
+			// 			</div>
+			// 		</div>
+			// 		<br />
+			// 		<br />
 	
-					<div className="category-container">
-						<div id="nutty-casino" className="page-category">
-							NUTTY CASINO
-						</div>
-					</div>
-					<div id="board-games" className="game-browser">
-						<div id="game-blackjack" className="game">
-							<div id="blackjack-menu" className="game-menu">
-								<div id="fish-thumbnail" className="thumbnail">
+			// 		<div className="category-container">
+			// 			<div id="nutty-casino" className="page-category">
+			// 				NUTTY CASINO
+			// 			</div>
+			// 		</div>
+			// 		<div id="board-games" className="game-browser">
+			// 			<div id="game-blackjack" className="game">
+			// 				<div id="blackjack-menu" className="game-menu">
+			// 					<div id="fish-thumbnail" className="thumbnail">
 	
-								</div>
-							</div>
-							<div className="game-information">
-								<div className="game-title">
-									BLACKJACK
-								</div>
-								<div className="button-container">
-									<button className="play-button">
-										PLAY NOW
-									</button>
-								</div>
-								<div className="player-counter-container">
-									<div className="player-counter-title">
-										PLAYERS ACTIVE
-									</div>
-									<div className="player-counter">
-										23
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+			// 					</div>
+			// 				</div>
+			// 				<div className="game-information">
+			// 					<div className="game-title">
+			// 						BLACKJACK
+			// 					</div>
+			// 					<div className="button-container">
+			// 						<button className="play-button">
+			// 							PLAY NOW
+			// 						</button>
+			// 					</div>
+			// 					<div className="player-counter-container">
+			// 						<div className="player-counter-title">
+			// 							PLAYERS ACTIVE
+			// 						</div>
+			// 						<div className="player-counter">
+			// 							23
+			// 						</div>
+			// 					</div>
+			// 				</div>
+			// 			</div>
+			// 		</div>
+			// 	</div>
+			// </div>
 		);
     }
 }
