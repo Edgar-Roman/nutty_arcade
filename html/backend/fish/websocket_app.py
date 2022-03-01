a2d2a95c0a12529e22fea263e7ab862efbc19a54
deanzhou <dzhouwave@gmail.com>
2022-03-01 09:17:43 -0800

    websocket 2player prototype

--- html/backend/fish/websocket_app.py
+++ html/backend/fish/websocket_app.py
@@ -5,58 +5,138 @@ import json
 
 import asyncio
 import websockets
+import secrets
 
 game = None
 
-def get_hand(status=""):
+JOIN = {}
+
+def get_hand(player, status=""):
     global game
-    hand, num_cards, teamScore, opponentScore, currentPlayer, history = game.getGameState(0)
+    hand, num_cards, teamScore, opponentScore, currentPlayer, history = game.getGameState(player)
     cards = [card[0] + str(card[1]) for card in hand]
-    return json.dumps({
+    return {
         "status": status,
+        "playerID": player,
         "hand": ['./cards/' + card + '.png' for card in cards],
         "numCards": [int(num) for num in num_cards],
         "teamScore": teamScore,
         "opponentScore": opponentScore,
         "currentPlayer": currentPlayer,
         "history": history
-    })
+    }
 
 def start_game():
     global game
     game = Fish()
     return get_hand()
 
-def askCard():
+def askCard(player, card, other_player):
     global game
-    card = request.args.get('card')
-    player = request.args.get('player')
-    status = game.askCard(card[0], int(card[1:]), 0, int(player))
+    status = game.askCard(card[0], int(card[1:]), player, int(other_player))
     return get_hand(status)
 
-def declareSuit():
+def declareSuit(player, suit, id1, id2, id3, id4, id5, id6):
     global game
-    suit = request.args.get('suit')
-    declare_id = 0
-    id1 = request.args.get('id1')
-    id2 = request.args.get('id2')
-    id3 = request.args.get('id3')
-    id4 = request.args.get('id4')
-    id5 = request.args.get('id5')
-    id6 = request.args.get('id6')
-    status = game.declareSuit(int(suit), int(declare_id), int(id1), int(id2), int(id3), int(id4), int(id5), int(id6))
+    status = game.declareSuit(int(suit), player, int(id1), int(id2), int(id3), int(id4), int(id5), int(id6))
     return get_hand(status)
 
-def passTurn():
+def passTurn(player, teammate):
     global game
-    teammate = request.args.get('teammate')
-    status = game.passTurn(0, int(teammate))
+    status = game.passTurn(player, int(teammate))
     return get_hand(status)
+
+async def error(websocket, message):
+    event = {
+        "type": "error",
+        "message": message,
+    }
+    await websocket.send(json.dumps(event))
+
+async def play(websocket, game, player, connected):
+    """
+    Receive and process moves from a player.
+    """
+    async for message in websocket:
+        # parse input from UI
+        event = json.loads(json.loads(message))
+        print(type(event), event)
+        if event["type"] == "askCard":
+            card = event["card"]
+            other_player = event["player"]
+            askCard(player, card, other_player)
+        elif event["type"] == "declareSuit":
+            suit = event["suit"]
+            id1 = event["id1"]
+            id2 = event["id2"]
+            id3 = event["id3"]
+            id4 = event["id4"]
+            id5 = event["id5"]
+            id6 = event["id6"]
+            declareSuit(player, suit, id1, id2, id3, id4, id5, id6)
+        elif event["type"] == "passTurn":
+            teammate = event["teammate"]
+            passTurn(player, teammate)
+        elif event["type"] == "getHand":
+            pass
+        else:
+            assert False # input wrong type?
+        for connection in connected:
+            gameState = get_hand(player)
+            await connection.send(json.dumps(gameState))
+
+async def start(websocket): # newer vewsion of start_game() ?
+    """
+    Handle a connection from the first player: start a new game.
+    """
+    global game
+    game = Fish()
+    connected = {websocket}
+    
+    join_key = secrets.token_urlsafe(12)
+    JOIN[join_key] = game, connected
+
+    try:
+        # Send the secret access tokens to the browser of the first player,
+        # where they'll be used for building "join" links
+
+        event = {
+            "type": "init",
+            "join": join_key,
+        }
+        await websocket.send(json.dumps(event))
+        # Receive and process moves from the first player.
+        await play(websocket, game, 0, connected)
+    finally:
+        del JOIN[join_key]
+    
+async def join(websocket, join_key):
+    """
+    Handle a connection from the second player: join an existing game
+    """
+    # Find the Fish game.
+    try:
+        game, connected = JOIN[join_key]
+    except KeyError:
+        await error(websocket, "Game not found.")
+        return
+
+    # Register to receive moves from this game
+    connected.add(websocket)
+    await play(websocket, game, 1, connected)
 
 async def handler(websocket):
     while True:
         message = await websocket.recv()
-        print(message)
+        event = json.loads(json.loads(message))
+        print(type(event), event)
+        assert event["type"] == "init"
+
+        if "join" in event:
+            # Second player joins an existing game
+            await join(websocket, event["join"])
+        else:
+            await start(websocket)
 
 async def main():
     async with websockets.serve(handler, "", 5000):
